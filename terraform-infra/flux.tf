ca2a4abb5b9648d3599eba25264b4761a4695b63
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-02-26 19:32:05 +0100

    Add new app

--- terraform-infra/flux.tf
+++ terraform-infra/flux.tf
@@ -3,7 +3,7 @@ resource "flux_bootstrap_git" "this" {
   path               = "clusters/my-cluster"
 }
 
-resource "kubernetes_manifest" "flux_kustomization" {
+resource "kubernetes_manifest" "flux_kustomization_game_2048" {
   manifest = {
     apiVersion = "kustomize.toolkit.fluxcd.io/v1"
     kind = "Kustomization"
@@ -19,6 +19,28 @@ resource "kubernetes_manifest" "flux_kustomization" {
         name = "flux-system"
       }
       path = "./game-2048/kustomize"
+      prune = "true"
+      timeout = "1m"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "flux_kustomization_emojivoto" {
+  manifest = {
+    apiVersion = "kustomize.toolkit.fluxcd.io/v1"
+    kind = "Kustomization"
+    metadata = {
+      name = "emojivoto"
+      namespace = "emojivoto"
+    }
+    spec = {
+      interval = "10m"
+      targetNamespace = "emojivoto"
+      sourceRef = {
+        kind = "GitRepository"
+        name = "flux-system"
+      }
+      path = "./emojivoto-example/kustomize"
       prune = "true"
       timeout = "1m"
     }
