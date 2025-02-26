05e9ef262366de9a154efb3d4bc2abe52586a82e
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-02-26 19:33:44 +0100

    Add namespaces

--- terraform-infra/flux.tf
+++ terraform-infra/flux.tf
@@ -3,45 +3,57 @@ resource "flux_bootstrap_git" "this" {
   path               = "clusters/my-cluster"
 }
 
+resource "kubernetes_namespace" "game_2048" {
+  metadata {
+    name = "game-2048"
+  }
+}
+
 resource "kubernetes_manifest" "flux_kustomization_game_2048" {
   manifest = {
     apiVersion = "kustomize.toolkit.fluxcd.io/v1"
-    kind = "Kustomization"
+    kind       = "Kustomization"
     metadata = {
-      name = "game-2048"
+      name      = "game-2048"
       namespace = "flux-system"
     }
     spec = {
-      interval = "10m"
+      interval        = "10m"
       targetNamespace = "game-2048"
       sourceRef = {
         kind = "GitRepository"
         name = "flux-system"
       }
-      path = "./game-2048/kustomize"
-      prune = "true"
+      path    = "./game-2048/kustomize"
+      prune   = "true"
       timeout = "1m"
     }
+  }
+}
+
+resource "kubernetes_namespace" "emojivoto" {
+  metadata {
+    name = "emojivoto"
   }
 }
 
 resource "kubernetes_manifest" "flux_kustomization_emojivoto" {
   manifest = {
     apiVersion = "kustomize.toolkit.fluxcd.io/v1"
-    kind = "Kustomization"
+    kind       = "Kustomization"
     metadata = {
-      name = "emojivoto"
+      name      = "emojivoto"
       namespace = "emojivoto"
     }
     spec = {
-      interval = "10m"
+      interval        = "10m"
       targetNamespace = "emojivoto"
       sourceRef = {
         kind = "GitRepository"
         name = "flux-system"
       }
-      path = "./emojivoto-example/kustomize"
-      prune = "true"
+      path    = "./emojivoto-example/kustomize"
+      prune   = "true"
       timeout = "1m"
     }
   }
