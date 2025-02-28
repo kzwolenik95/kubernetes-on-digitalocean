7ea3eff8f60d95dadcf2da165e17caec928bf005
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-02-28 13:55:06 +0100

    remove unsused manifests

--- terraform-infra/kube-prometheus.tf
+++ terraform-infra/kube-prometheus.tf
@@ -31,7 +31,7 @@ resource "kubernetes_manifest" "flux_kustomization_kube_prometheus" {
       namespace = "flux-system"
     }
     spec = {
-      interval = "10m"
+      interval        = "10m"
       sourceRef = {
         kind = "GitRepository"
         name = "kube-prometheus"
@@ -42,56 +42,3 @@ resource "kubernetes_manifest" "flux_kustomization_kube_prometheus" {
     }
   }
 }
-
-resource "kubernetes_manifest" "grafana_ingress" {
-  manifest = {
-    "apiVersion" = "networking.k8s.io/v1"
-    "kind"       = "Ingress"
-    "metadata" = {
-      "annotations" = {
-        "external-dns.alpha.kubernetes.io/hostname" = "grafana.kzwolenik.com"
-      }
-      "name" = "grafana-ingress"
-    }
-    "spec" = {
-      "ingressClassName" = "kong"
-      "rules" = [
-        {
-          "host" = "grafana.kzwolenik.com"
-          "http" = {
-            "paths" = [
-              {
-                "backend" = {
-                  "service" = {
-                    "name" = "grafana"
-                    "port" = {
-                      "number" = 3000
-                    }
-                  }
-                }
-                "path"     = "/"
-                "pathType" = "ImplementationSpecific"
-              },
-            ]
-          }
-        },
-      ]
-    }
-  }
-}
-
-# resource "kubernetes_manifest" "kongplugin_ip_restriction" {
-#   manifest = {
-#     "apiVersion" = "configuration.konghq.com/v1"
-#     "config" = {
-#       "allow" = [
-#         "your.ip.address.here",
-#       ]
-#     }
-#     "kind" = "KongPlugin"
-#     "metadata" = {
-#       "name" = "grafana-ip-restriction"
-#     }
-#     "plugin" = "ip-restriction"
-#   }
-# }
