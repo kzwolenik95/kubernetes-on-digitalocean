f16c6dfbaa3a4bba3c46440055bd9580a6e77de2
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-03-16 17:00:57 +0100

    Add vault iac

--- vault-iac/initial-configuration/initial-configuration.tf
+++ vault-iac/initial-configuration/initial-configuration.tf
@@ -46,10 +46,22 @@ resource "kubernetes_role" "terraform-vault-role" {
   }
 
   rule {
+    api_groups = ["coordination.k8s.io"]
+    resources  = ["leases"]
+    verbs      = ["create", "get", "list", "update", "delete", "watch"]
+  }
+
+  rule {
+    api_groups     = [""]
+    resources      = ["secrets"]
+    verbs          = ["list", "create"]
+  }
+
+  rule {
     api_groups     = [""]
     resources      = ["secrets"]
     resource_names = ["tfstate-default-terraform"]
-    verbs          = ["get", "create", "update", "patch"]
+    verbs          = ["get", "update", "patch"]
   }
 }
 
@@ -74,7 +86,31 @@ resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
 
-resource "vault_kubernetes_auth_backend_config" "example" {
+resource "vault_kubernetes_auth_backend_config" "terraform-vault-config" {
   backend         = vault_auth_backend.kubernetes.path
   kubernetes_host = "https://kubernetes.default.svc"
+}
+
+resource "vault_policy" "terraform-vault-policy" {
+  name = "terraform-vault"
+
+  policy = <<-EOT
+    path "*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+
+    path "sys/*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "terraform-vault-role" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "terraform"
+  bound_service_account_names      = [kubernetes_service_account.terraform-vault.metadata.name]
+  bound_service_account_namespaces = ["hashicorp-vault"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.terraform-vault-policy.name]
+  # audience                         = "vault"
 }
---
+++ vault-iac/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "kubernetes" {
+    secret_suffix = "terraform"
+    namespace     = "hashicorp-vault"
+  }
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.7.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "http://vault.hashicorp-vault.svc.cluster.local:8200"
+  auth_login {
+    path = "auth/kubernetes/login"
+
+    parameters = {
+      role = "terraform"
+      jwt  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")
+    }
+  }
+}
+
+resource "vault_kubernetes_auth_backend_role" "webapp" {
+  backend                          = "kubernetes"
+  role_name                        = "webapp"
+  bound_service_account_names      = ["vault-auth"]
+  bound_service_account_namespaces = ["default"]
+  token_policies                   = ["demo-policy"]
+  token_ttl                        = 259200 # 72 hours in seconds
+}
+
+resource "vault_policy" "demo_policy" {
+  name = "demo-policy"
+
+  policy = <<-EOT
+    path "demo-app/*" {
+      capabilities = ["read"]
+    }
+    EOT
+}
+
+resource "vault_mount" "demo_app" {
+  path = "demo-app"
+  type = "kv"
+  options = {
+    version = "2"
+  }
+}
