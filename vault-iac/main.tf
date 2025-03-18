8e99de20c343751c7182e96d05f497b4894583ba
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-03-18 18:36:49 +0100

    Configure nextcloud secret

--- vault-iac/main.tf
+++ vault-iac/main.tf
@@ -23,29 +23,29 @@ provider "vault" {
   }
 }
 
-resource "vault_kubernetes_auth_backend_role" "webapp" {
-  backend                          = "kubernetes"
-  role_name                        = "webapp"
-  bound_service_account_names      = ["vault-auth"]
-  bound_service_account_namespaces = ["default"]
-  token_policies                   = ["demo-policy"]
-  token_ttl                        = 259200 # 72 hours in seconds
-}
-
-resource "vault_policy" "demo_policy" {
-  name = "demo-policy"
-
-  policy = <<-EOT
-    path "demo-app/*" {
-      capabilities = ["read"]
-    }
-    EOT
-}
-
-resource "vault_mount" "demo_app" {
-  path = "demo-app"
+resource "vault_mount" "nextcloud_secret_engine" {
+  path = "nextcloud"
   type = "kv"
   options = {
     version = "2"
   }
+}
+
+resource "vault_kubernetes_auth_backend_role" "nextcloud_role" {
+  backend                          = "kubernetes"
+  role_name                        = "nextcloud"
+  bound_service_account_names      = ["nextcloud"]
+  bound_service_account_namespaces = ["nextcloud-storage"]
+  token_policies                   = ["nextcloud-policy"]
+  token_ttl                        = 3600
+}
+
+resource "vault_policy" "nextcloud_policy" {
+  name = "nextcloud-policy"
+
+  policy = <<-EOT
+    path "nextcloud/*" {
+      capabilities = ["read"]
+    }
+    EOT
 }
